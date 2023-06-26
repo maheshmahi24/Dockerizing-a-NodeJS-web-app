6b846f5f6061531a4d2debe130c4994925ec106e
maheshmahi24 <133503963+maheshmahi24@users.noreply.github.com>
2023-06-26 13:25:51 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ docker pull 092101872227.dkr.ecr.ap-southeast-1.amazonaws.com/automation-docker:
 docker ps -a
 docker rm -f dotnet-app | ls -la
 docker ps -a
-docker run -itd -p 3000:3000 --name node:10 092101872227.dkr.ecr.ap-southeast-1.amazonaws.com/automation-docker:$1
+docker run -itd -p 3001:3001 --name node:10 092101872227.dkr.ecr.ap-southeast-1.amazonaws.com/automation-docker:$1
 docker ps -a
